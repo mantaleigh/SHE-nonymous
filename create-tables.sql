abf795dc680be18f3e04567a1116aa968795b3fb
mantaleigh <samantha@samanthavoigt.com>
2016-04-19 12:25:44 -0400

    fixed sql errors

--- create-tables.sql
+++ create-tables.sql
@@ -2,22 +2,22 @@
 -- last modified: 4/16/16
 -- DDL statements to create the tables for the SHE-nonymous project
 
-
 -- TODO: Make sure that referential integrity works out here
 -- TODO: Make sure that deleting a question in the optional_questions table will delete all the answers in the optional answers table... but do we really want to loose all that data?  Or take it upon the SHE user to save the information 
 
+use svoigt_db;
 
 drop table if exists login; 
 drop table if exists questions; 
-drop table if exists optional_questions; 
 drop table if exists optional_answers; 
+drop table if exists optional_questions; 
 
 -- create the login table
 -- The ENGINE=InnoDB allows enforcement of foreign key constraints.
 
 create table login(
     login_id varchar(50) not null primary key, 
-    passhash char(32), -- for sha256 hash
+    passhash char(32) -- for sha256 hash
  );
 
 -- Define the questions table
